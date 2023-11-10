7b3aea44e158a777cf9c00fa098a0fe2816d2eee
Mark Briscoe <mark@mcmw.co.uk>
2023-11-10 10:00:34 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+
+fs.readdir(process.cwd(), (err, filenames) => {
+    if (err) {
+        throw new Error(err);
+    }
+
+    console.log(filenames);
+});
