c300050a2c84847b858e717a176821f8888e03a5
mwbCoding <mwbriscoe@gmail.com>
2023-11-15 12:55:30 +0000

    test commit

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ fs.readdir(targetDir, async (err, filenames) => {
         console.log(err);
     }
 
-    const statPromises = filenames.map((filename) => {
+    const statPromises = filenames.map(filename => {
         return lstat(path.join(targetDir, filename));
     });
 
@@ -30,16 +30,3 @@ fs.readdir(targetDir, async (err, filenames) => {
         console.log(stats.isFile() ? filenames[index] : chalk.yellow(chalk.bold(filenames[index])));
     }
 });
-
-// Method #1
-// const lstat = filename => {
-//   return new Promise((resolve, reject) => {
-//     fs.lstat(filename, (err, stats) => {
-//       if (err) {
-//         reject(err);
-//       }
-
-//       resolve(stats);
-//     });
-//   });
-// };
